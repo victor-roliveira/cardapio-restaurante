59de346d14c8bdb8456c796af0ca15502332952f
VictoROliveira-Dev <victorramos123@outlook.com>
2025-02-27 14:06:39 -0300

    feat: create first carousel in pizza section on homepage

--- src/app/cardapio/page.tsx
+++ src/app/cardapio/page.tsx
@@ -16,6 +16,14 @@ import logoRestaurante from "../assets/logo-restaurante.png";
 import pizza from "../assets/pizza-image.png";
 import massa from "../assets/macarrao-bacon.jpg";
 import drink from "../assets/aperol-zcafe.jpg";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
 
 const Cardapio = () => {
   return (
@@ -89,46 +97,45 @@ const Cardapio = () => {
           <div className="w-full h-[400px] bg-black text-white pt-10">
             <div className="flex flex-col items-center justify-center gap-5">
               <Image src={logoRestaurante} alt="Logo do restaurante" />
-              <h2 className="text-4xl font-bold">Alguns dos nossos pratos!</h2>
+              <h2 className="text-2xl font-bold">
+                Há mais de 10 anos oferecendo o melhor para você.
+              </h2>
               <p className="text-sm text-white/75">
-                Caso queira visualizar mais itens, escolha através do menu ao
-                lado.
+                Acesse mais dos nossos produtos através do menu ao lado.
               </p>
             </div>
           </div>
           <div className="flex flex-col items-center text-white text-center">
             <p className="text-white text-2xl font-semibold py-2">Pizzas</p>
             <div className="flex gap-4">
-              <div className="flex flex-col">
-                <Image
-                  src={pizza}
-                  alt="Imagem das promocoes"
-                  className="w-[250px] h-[200px] rounded-sm"
-                />
-                <div className="bg-black w-full">
-                  <p>Promoção 1</p>
-                </div>
-              </div>
-              <div className="flex flex-col">
-                <Image
-                  src={pizza}
-                  alt="Imagem das promocoes"
-                  className="w-[250px] h-[200px] rounded-sm"
-                />
-                <div className="bg-black w-full">
-                  <p>Promoção 2</p>
-                </div>
-              </div>
-              <div className="flex flex-col">
-                <Image
-                  src={pizza}
-                  alt="Imagem das promocoes"
-                  className="w-[250px] h-[200px] rounded-sm"
-                />
-                <div className="bg-black w-full">
-                  <p>Promoção 3</p>
-                </div>
-              </div>
+              <Carousel className="mx-15">
+                <CarouselContent>
+                  {Array(3)
+                    .fill(null)
+                    .map((_, index) => (
+                      <CarouselItem key={index} className="basis-1/3">
+                        <Card className="w-[200px]">
+                          <CardContent className="w-full flex flex-col justify-center p-0">
+                            <Image
+                              src={pizza}
+                              alt={`Imagem pizza ${index + 1}`}
+                              className="w-full rounded-t-lg"
+                            />
+                            <h2 className="p-1 text-lg font-semibold">
+                              Pizza Peperoni
+                            </h2>
+                            <p className="text-sm p-1 text-gray-500">
+                              Ingredientes: Mussarela, Molho pomodoro, Orégano,
+                              Peperoni
+                            </p>
+                          </CardContent>
+                        </Card>
+                      </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious className="text-black" />
+                <CarouselNext className="text-black" />
+              </Carousel>
             </div>
           </div>
           <div className="flex flex-col items-center text-white text-center">
