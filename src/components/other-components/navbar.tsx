ff19c0a99beb741012f2a648980c47332e109977
VictoROliveira-Dev <victorramos123@outlook.com>
2025-03-20 16:46:03 -0300

    chore: adjust input search

--- src/components/other-components/navbar.tsx
+++ src/components/other-components/navbar.tsx
@@ -1,6 +1,6 @@
 import { Search } from "lucide-react";
-import iconOffer from "../../app/assets/offer.svg";
 import iconWallet from "../../app/assets/wallet.svg";
+import bellIcon from "../../app/assets/bell-icon.svg";
 
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
@@ -10,9 +10,7 @@ import logoHome from "../../app/assets/icon-home.svg";
 import Image from "next/image";
 import CartMenu from "./cart-menu";
 
-
 const Navbar = () => {
-
   return (
     <nav className="flex items-center justify-between h-[70px] max-h-[70px] bg-black text-white">
       <div className="flex items-center gap-4">
@@ -21,22 +19,29 @@ const Navbar = () => {
           <Image src={logoHome} alt="logo home" width={30} />
         </Link>
       </div>
-      <div className="relative w-[250px]">
-        <Input
-          type="text"
-          placeholder="Busque o produto..."
-          className="w-full pl-10 border-2"
-        />
-        <Search className="absolute left-2 top-1/2 transform -translate-y-1/2" />
+      <div className="border-b p-1">
+        <div className="relative w-[250px]">
+          <Input
+            type="text"
+            placeholder="Busque o produto..."
+            className="w-full pl-10 border-none shadow-none"
+          />
+          <Search className="absolute left-2 top-1/2 transform -translate-y-1/2" />
+        </div>
       </div>
+
       <div className="flex items-center relative">
-        <Button className="h-[70px] bg-orange-500 rounded-none border-l border-r">
-          Ofertas
-          <Image src={iconOffer} alt="Ícone ofertas" width={20} />
+        <Button className="h-[70px]  bg-orange-500 rounded-none border-l border-r text-center break-words">
+          <span>
+            Chamar
+            <br />
+            Garçom
+          </span>
+          <Image src={bellIcon} alt="Ícone chamar garçom" width={20} />
         </Button>
         <CartMenu />
-        <Button className="h-[70px] bg-orange-500 rounded-none">
-          Conta
+        <Button className="h-[70px] bg-orange-500 rounded-none text-center break-words">
+          <span className="block text-sm leading-tight">Conta</span>
           <Image src={iconWallet} alt="Ícone carteira" width={20} />
         </Button>
       </div>
